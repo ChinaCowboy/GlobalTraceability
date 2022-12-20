57c2c5ec5397759ad135219b5c3dcbcb136e8d2c
Bing Niu <bing.niu@marel.com>
2022-12-21 07:21:57 +0800

    add style

--- HelloAngular/src/app/app.module.ts
+++ HelloAngular/src/app/app.module.ts
@@ -9,6 +9,15 @@ import { SearchComponent } from './search/search.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule} from "@angular/common/http";
 import { EditComponent } from './edit/edit.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,7 +30,14 @@ import { EditComponent } from './edit/edit.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+  MatInputModule,
+MatListModule,
+MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- HelloAngular/src/app/edit/edit.component.spec.ts
+++ HelloAngular/src/app/edit/edit.component.spec.ts
@@ -1,14 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { MockActivatedRoute } from '../shared/search/mocks/routes';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { EditComponent } from './edit.component';
 
 describe('EditComponent', () => {
   let component: EditComponent;
   let fixture: ComponentFixture<EditComponent>;
+  let mockActivatedRoute: MockActivatedRoute;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EditComponent ]
+      declarations: [ EditComponent ],
+      providers: [
+        {provide: ActivatedRoute, useValue: mockActivatedRoute}
+        ],
+      imports: [ HttpClientTestingModule]
+ 
     })
     .compileComponents();
 
